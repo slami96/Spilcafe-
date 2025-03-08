9cd50f24c25041e2fdf444c36220f49231926e43
slami96 <adam.slamen@gmail.com>
2025-03-08 20:15:44 +0100

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -4,7 +4,7 @@ import GameList from "./GameList";
 export default function Admin() {
   return (
     <div>
-      <img src="/images/spilcafe_logo.png" alt="Board Game Cafe Logo" className="cafe-logo" />
+      <img src="/images/spilcafe_logo .png" alt="Board Game Cafe Logo" className="cafe-logo" />
       <GameList />
     </div>
   );
