5191a6689d7b0a67826e354ff9120a127cfaac2a
slami96 <adam.slamen@gmail.com>
2025-03-09 20:52:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Admin from "./components/Admin";
+import { AuthProvider } from './contexts/AuthContext';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Admin />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Admin />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
