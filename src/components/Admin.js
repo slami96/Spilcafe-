27c853a7f6f676e6b1e9b4f9fa94415b66c966d7
slami96 <adam.slamen@gmail.com>
2025-03-09 21:53:03 +0100

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import GameList from "./GameList";
+import LogoutButton from "./LogoutButton";
 
 export default function Admin() {
   return (
     <div>
-      <img src="/images/spilcafe_logo .png" alt="Board Game Cafe Logo" className="cafe-logo" />
+      <div className="admin-header">
+        <img src="/images/spilcafe_logo.png" alt="Board Game Cafe Logo" className="cafe-logo" />
+        <LogoutButton />
+      </div>
       <GameList />
     </div>
   );
